4c0d3f185998d66a7d4bc7e2da991d81f8b96246
geobas <ksenera@yahoo.com>
2016-10-14 21:25:15 +0300

    Rendering a json response

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,6 +5,7 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+// use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,6 +14,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        // return new Response('Hello !!!');
+
+        /* $data = array(
+                    'fname' => "Geo",
+                    'lname' => "Bas",
+                    'state' => "Athens",
+                );
+
+        $data = json_encode($data);
+
+        $response = new Response($data);
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response; */
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
